d08c49752c55fb286e851e5ae2119e13e32a85c2
Alex Gill <alex.gill@neo4j.com>
2026-05-07 22:13:55 +0100

    feat: add key binding definitions

--- keys.go
+++ keys.go
@@ -1,1 +1,44 @@
 package main
+
+import "github.com/charmbracelet/bubbles/key"
+
+type keyMap struct {
+	Up          key.Binding
+	Down        key.Binding
+	Delete      key.Binding
+	Enter       key.Binding
+	Escape      key.Binding
+	PauseResume key.Binding
+	Quit        key.Binding
+}
+
+var keys = keyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up"),
+		key.WithHelp("↑", "up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down"),
+		key.WithHelp("↓", "down"),
+	),
+	Delete: key.NewBinding(
+		key.WithKeys("backspace"),
+		key.WithHelp("backspace", "delete snag"),
+	),
+	Enter: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "add snag"),
+	),
+	Escape: key.NewBinding(
+		key.WithKeys("esc"),
+		key.WithHelp("esc", "clear / quit"),
+	),
+	PauseResume: key.NewBinding(
+		key.WithKeys("ctrl+p"),
+		key.WithHelp("ctrl+p", "pause/resume"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c"),
+		key.WithHelp("ctrl+c", "quit"),
+	),
+}
